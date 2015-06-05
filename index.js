3b27e8ca5eb64b1c6152d870d9859dbd81500921
David Weinstein <dweinst@insitusec.com>
2015-06-05 17:36:34 -0400

    cleanup

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var Parser = new TSV.Parser("\t");
 var indexBy = require('lodash.indexby');
 var find = require('lodash.find');
 
-
-
 var data = fs.readFileSync(__dirname+'/data/mnclist.tsv').toString();
 var parsed = Parser.parse(data).slice(0,-1);
 
